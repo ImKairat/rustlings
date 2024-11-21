5f0b82c51e2ad425f5507986a4bbd6fd614338bb
Kubanychbek uulu Kairat <kubanychbekukairat@gmail.com>
2024-11-21 12:42:02 +0600

    [ADDED] exercises/10_modules/modules4.rs:                     - TODO comment describing the issue.

---
+++ exercises/10_modules/modules4.rs
@@ -0,0 +1,5 @@
+// TODO: Hi, I would like to add more exercises about modules that involve the declaration
+//  of modules in subdirectories and separate files. In this way, students can make sure
+//  they understand better scope and privacy concepts and rules around the naming of directories
+//  and files. Are you OK with that? Thank you
+
